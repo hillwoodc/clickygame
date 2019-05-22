dd5adf5b7cfde869808a4c09c8a921a562e7eda1
Hilwood Chan <hillwoodc@gmail.com>
2019-05-22 15:10:44 -0700

    final commit

---
+++ build/precache-manifest.8317e96b2564923d99f5b5b1da92d7ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c080011ebbb77bc1f867baf37f4a68b7",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "7c2a14a972ea2cd8a9fb",
+    "url": "/clickygame/static/css/main.b9627506.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/clickygame/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "7c2a14a972ea2cd8a9fb",
+    "url": "/clickygame/static/js/main.0f80f18f.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
